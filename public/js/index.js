505f4dac72ab5ddeec1ed9e2a2db6341cdb7b25b
nazmicancalik <nazmicancalik@gmail.com>
2018-06-02 18:09:47 +0300

    Message events are added.

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+
+socket.on("connect", function() {
+  console.log("Connected to the server");
+  socket.emit("createMessage", {
+    from: "Andrew",
+    text: "That works for me"
+  });
+});
+
+socket.on("disconnect", function() {
+  console.log("Disconnected from server");
+});
+
+socket.on("newMessage", function(message) {
+  console.log("newMessage", message);
+});
--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected");
 
+  socket.on("createMessage", message => {
+    console.log("Create Message", message);
+  });
+
+  socket.emit("newMessage", {
+    from: "John",
+    text: "See you later",
+    createdAt: 123
+  });
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
